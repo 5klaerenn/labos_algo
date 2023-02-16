2c611a59e368155a547e555c5f5ec1cba4aeb455
Sklaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-02-16 14:37:40 -0500

    Trois premiers exercices. Sans méthodes pour l'instant

---
+++ Labo2_906.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labo2_906;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sklaerenn
+ */
+public class Labo2_906 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        
+        /*
+        Exercice 1:
+        Écrire un algorithme portant sur les structures de répétition et faites en sorte que l’usager puisse fournir lui-même la valeur supérieure de la boucle, et affiche ensuite tous les chiffres entre 0 et cette valeur
+        */
+       
+            
+//        System.out.println("Entrez un nombre");
+//        int n=sc.nextInt();
+//        byte  i;
+//        
+//        for (i=0; i <= n; i++){
+//                System.out.println(i);
+//        }
+        
+        /*
+        Exercice 2:
+Écrire un algorithme qui demande un nombre entier positif à l’usager et qui calcule ensuite la somme de tous les nombres plus petit ou égaux au nombre entré. Par exemple, si l’usager entre le chiffre 8, votre programme devra afficher 36 car  8+7+6+5+4+3+2+1 = 36.
+Appeler une fonction pour retourner le resultat
+
+       
+        
+        System.out.println("Entrez un nombre entier positif");
+        int n = sc.nextInt();
+        int i;
+        int somme =0;
+        
+        while (n < 0) {            
+             System.out.println("Entrez un nombre entier positif");
+             n = sc.nextInt();
+        }
+        for (i=n; i>0; i--) {
+            somme = somme + i;
+        }         
+        
+        System.out.println(somme);
+*/        
+        
+        /*
+        Exercice 3 :
+Écrire un algorithme qui demande un nombre entier positif à l’usager et qui calcule ensuite le produit de tous les nombres plus petit ou égaux au nombre entré. 
+
+        
+        
+        System.out.println("Entrez un nombre entier positif");
+        int n = sc.nextInt();
+        int i;
+        int produit = 1;
+        
+        while(n < 0){
+            System.out.println("Entrez un nombre entier positif");
+            n = sc.nextInt();
+        }
+        
+        for(i = n; i > 0; i--){
+            produit = produit * i;
+        }
+        
+        System.out.println(produit);
+      
+*/
+        
+        /*
+       Exercice 4 :
+Écrire un algorithme qui demande à l’usager d’entrer un nombre et qui affiche tous les nombres pairs inférieur ou égal à la valeur entrée dans l’ordre décroissant. Par exemple, si on entre 13, on veut voir afficher 12 10 8 6 4 2.
+        */
+        
+        
+             
+        
+    }  
+}
