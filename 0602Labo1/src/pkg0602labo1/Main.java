b77ab7d779b5ae2c928fb609f1e533a839c27140
Sklaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-03-03 08:54:33 -0500

    Initialisation de l'exercice 2 (variables) et creation de la fonction pour la vérification des nombres entiers

--- 0602Labo1/src/pkg0602labo1/Main.java
+++ 0602Labo1/src/pkg0602labo1/Main.java
@@ -17,6 +17,8 @@ public class Main {
      */
     public static void main(String[] args) {
         
+        Scanner sc=new Scanner(System.in);
+        
         /* 
     ============================================================================    
             EXERCICE 1 
@@ -24,19 +26,19 @@ public class Main {
         le prénom et l’année de naissance d’une personne, ensuite il affiche le 
         nom, le prénom et l’âge de cette personne.
     ============================================================================
-        */
+        
         
     String nom, prenom;
     int year;
     int currentYear = 2023;
-    Scanner sc=new Scanner(System.in);
+    
     
         System.out.println("Entrez votre nom");
         nom = sc.nextLine();
         System.out.println("Entrez votre prenom");
         prenom = sc.nextLine();
         System.out.println("Entrez votre année de naissance");
-        year = sc.nextInt();
+        year = estEntier();
         
         if(year < currentYear && year > 1900){
             System.out.println("Votre nom est : "+nom);
@@ -45,11 +47,42 @@ public class Main {
         } else {
             System.out.println("Erreur dans la date");
         }
+*/
         
+        /* 
+    ============================================================================    
+            EXERCICE 2 
+        Modifier l’algorithme de la calculatrice simple afin qu’elle puisse 
+        effectuer en plus des additions et des soustractions, des opérations de 
+        multiplications et de divisions. Assurez-vous que l’on ne puisse diviser
+        par zéro.
+    ============================================================================
+        */
         
+        int nbr1, nbr2;
+        String entry;
+        char op; 
         
+        System.out.println("Entrez un premier nombre");
+        nbr1 = estEntier();
+        System.out.println("Entrez un second nombre");
+        nbr2 = estEntier();
+        System.out.println("Entrez votre operateur");
+        entry = sc.nextLine();
+        op = entry.charAt(0);
         
+        System.out.printf("%d %d %s", nbr1, nbr2, op);
         
     }
+    
+        static int estEntier(){
+        Scanner sc = new Scanner(System.in);
+        int n;            
+        while(!sc.hasNextInt()){
+            System.out.println("Erreur, entrez un nombre entier");
+            sc.next(); 
+            } 
+            return n = sc.nextInt();
+        }
     
 }
