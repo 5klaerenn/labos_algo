4131bde2bd6da89606002939bd1d6af9e1b9574c
Sklaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-03-03 08:35:43 -0500

    Ajoute de l'exercice 1

--- 0602Labo1/src/pkg0602labo1/Main.java
+++ 0602Labo1/src/pkg0602labo1/Main.java
@@ -4,6 +4,8 @@
  */
 package pkg0602labo1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Sklaerenn
@@ -14,7 +16,40 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        /* 
+    ============================================================================    
+            EXERCICE 1 
+        Écrire un algorithme (pseudo code) qui récupère le nom, 
+        le prénom et l’année de naissance d’une personne, ensuite il affiche le 
+        nom, le prénom et l’âge de cette personne.
+    ============================================================================
+        */
+        
+    String nom, prenom;
+    int year;
+    int currentYear = 2023;
+    Scanner sc=new Scanner(System.in);
+    
+        System.out.println("Entrez votre nom");
+        nom = sc.nextLine();
+        System.out.println("Entrez votre prenom");
+        prenom = sc.nextLine();
+        System.out.println("Entrez votre année de naissance");
+        year = sc.nextInt();
+        
+        if(year < currentYear && year > 1900){
+            System.out.println("Votre nom est : "+nom);
+            System.out.println("Votre prenom est : "+prenom);
+            System.out.printf("%s %d %s", "Vous avez ", currentYear-year, " ans");
+        } else {
+            System.out.println("Erreur dans la date");
+        }
+        
+        
+        
+        
+        
     }
     
 }
